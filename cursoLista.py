74164c0f1684594fd6e1fddf6df5be851c4a6b7d
allanpreis <allanpreis31@gmail.com>
2020-08-20 21:37:18 -0300

    validação do curso por estado concluida

--- cursoLista.py
+++ cursoLista.py
@@ -1,15 +1,32 @@
-def cursos_por_estado(estado: str) -> None:
+def cursos_por_estado(estado: str) -> str:
     """
-    Função que recebe uma a sigla de um estado e imprime na tela os cursos disponiveis naquele estado
+    Função que recebe uma a sigla de um estado, imprime na tela os cursos disponiveis naquele estado
+    pede ao usuario selecionar o curso que deseja(efetua a validação desse input) e retorna uma string com o codigo do curso selecionado
     :param estado: uma string contendo a sigla do estado a ser avaliado
-    :return: None
+    :return: string contendo o codigo do curso selecionado
     """
+    def select_curso(sg_estado: str) -> str:
+        curso = input("Escolha um dos cursos disponíveis: ").upper()
+        while True:
+            if curso_permi.get(curso):
+                if estado in curso_permi[curso]:
+                    break
+                print("\033[31mO código selecionado deve estar entre os mostrados na tabela acima.\033[m")
+            else:
+                print("\033[31mO valor digitado não correponde ao código de um curso\033[m")
+            curso = input("Escolha um dos cursos disponíveis: ").upper()
+        return curso
     estado = estado.upper()
     curso_permi = {
-        "A02": ["MA", "PI", "CE", "RN", "PE", "PB", "SE", "AL", "BA" ],
-        "A03": ["MT", "MS", "GO", "DF"],
-        "A04": ["AM", "RR", "AP", "PA", "TO", "RO", "AC","MG","RJ", "SP", "ES","PR", "RS", "SC"],
-        "A05": ["MG", "SP", "ES"]
+        "A01":("PR", "RS", "SC","MG","RJ",
+         "SP", "ES","MT", "MS", "GO","MA",
+          "PI", "CE", "RN", "PE", "PB",
+           "SE", "AL", "BA","AM", "RR", "AP",
+            "PA", "TO", "RO", "AC"),
+        "A02": ("MA", "PI", "CE", "RN", "PE", "PB", "SE", "AL", "BA" ),
+        "A03": ("MT", "MS", "GO", "DF"),
+        "A04": ("AM", "RR", "AP", "PA", "TO", "RO", "AC","MG","RJ", "SP", "ES","PR", "RS", "SC"),
+        "A05": ("MG", "SP", "ES")
     }
     cursos = {
         "A01": "AI e Machine Learing",
@@ -18,14 +35,14 @@ def cursos_por_estado(estado: str) -> None:
         "A04": "Programação Python",
         "A05": "Programação Java"
     }
-    
     print(34 * '-')
     print(f" CODIGO{'CURSO'.rjust(20)}")
     print(34 * '-')
 
     for codigo, curso in cursos.items():
         if codigo == "A01":
-            print(f"  {codigo.ljust(12)}{curso}")
+            if estado in curso_permi["A01"]:
+                print(f"  {codigo.ljust(12)}{curso}")
         elif codigo == "A02":
             if estado in curso_permi["A02"]:
                 print(f"  {codigo.ljust(12)}{curso}")
@@ -39,3 +56,5 @@ def cursos_por_estado(estado: str) -> None:
             if estado in curso_permi["A05"]:
                 print(f"  {codigo.ljust(12)}{curso}")
     print(34 * '-')
+
+    return select_curso(estado)
--- main.py
+++ main.py
@@ -14,19 +14,17 @@ while (sair != 4):
         print("\nInformações para a inscrição:")
         _nome = input("Nome do aluno: ")
         _cpf = 0
-        while not cpfValido.estaValido:
+        while cpfValido.estaValido != True:
             _cpf = input("CPF: ")
             cpfValido.validar(_cpf)
         _estado = ''
-        while not estadoValido.estaValido:
+        while estadoValido.estaValido != True:
             _estado = input("Estado que deseja fazer o curso: ").upper()
             estadoValido.validar(_estado)
         cursoLista.cursos_por_estado(_estado)
-        _curso = input("Escolha um dos cursos disponíveis: ")
         nome.append(_nome)
         cpf.append(_cpf)
         estado.append(_estado)
-        curso.append(_curso)
 
     elif menu == 2:
         opcao1 = int(input("1. Alterar inscrição pelo cpf\n2. Alterar inscrição pelo código de inscrição\n:"))
@@ -37,13 +35,7 @@ while (sair != 4):
         else:
             print("Opção Inválida")
     elif menu == 3:
-        matriz = [nome, cpf, estado, curso]
-        print("Imprimir lista aninhada: ")
-        for lista in matriz:
-            for elemento in lista:
-                print(elemento, end=' ')
-            print()
-
+        print("nada")
     elif menu == 4:
         break
     else:
