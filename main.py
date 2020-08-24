325a5167df2410e5a008f9ed1a32fa841040b9cb
cKassimiro <dihogo.teixeira@gmail.com>
2020-08-23 21:46:15 -0300

    Adicionando validaçao de CPF no main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import cursoLista
 import cpfValido
 import estadoValido
-import cursoLista
-
-from prettytable import PrettyTable
 
 codigo = []
 nome = []
@@ -19,10 +17,14 @@ while (sair != 4):
     if menu == 1:
         print("\nInformações para a inscrição:")
         _nome = input("Nome do aluno: ")
-        _cpf = 0
+
         while not cpfValido.estaValido:
             _cpf = input("CPF: ")
-            cpfValido.validar(_cpf)
+            if _cpf in cpf:
+                print("CPF já cadastrado.")
+            else:
+                cpfValido.validar(_cpf)
+
         _estado = ''
         while not estadoValido.estaValido:
             _estado = input("Estado que deseja fazer o curso: ").upper()
@@ -33,7 +35,7 @@ while (sair != 4):
         nome.append(_nome)
         cpf.append(_cpf)
         estado.append(_estado)
-        cpfValido.estaValido = False
+        cpfValido.estaValido= False
         estadoValido.estaValido = False
 
     elif menu == 2:
@@ -49,20 +51,9 @@ while (sair != 4):
             print(34 * '-')
             print(f" {codigoimport.ljust(0)} - {cursoimport}")
             print(34 * '-')
-            x = PrettyTable(["Codigo", "Nome", "Estado"])
-            x.align["Codigo"] = "l"
-            x.align["Nome"] = "l"
-            x.align["Estado"] = "l"
-            x.padding_width = 1
-            z = 0
-            while z < len(codigo):
-                x.add_row([codigo[z], nome[z], estado[z]])
-                z = z + 1
-            print(x)
+            print(str(codigo) + ' - ' + str(nome) + ' - ' + str(estado))
             print(34 * '-')
             print('Total de alunos: ', len(codigo), '\n')
-            
-          
     elif menu == 4:
         break
     else:
