52b08e7c2e89ecddc2dc27954eb51574d233f774
Allan Reis <47274495+allanpreis@users.noreply.github.com>
2020-08-25 12:17:41 -0300

    versão final

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import cursoLista
 import cpfValido
 import estadoValido
 
-codigo = []
-nome = []
 cpf = []
-estado = []
 curso = []
 alunos = []
 sair = 0
-# a variável i é utilizada como iteração para gerar o código de inscrição automático
 i = 1000
 
 while (sair != 4):
@@ -30,13 +26,9 @@ while (sair != 4):
         while not estadoValido.estaValido:
             _estado = input("Estado que deseja fazer o curso: ").upper()
             estadoValido.validar(_estado)
-        # recebe o retorno do curso da função cursos_por_estado
         curso = cursoLista.cursos_por_estado(_estado)
-        # incrementa para gerar o código da nova inscrição
         i += 1
-        # adiciona inscrição a lista alunos
         alunos.append([i, _nome, _cpf, _estado, curso])
-        # adiciona na lista para verificar duplicidade
         cpf.append(_cpf)
         cpfValido.estaValido = False
         estadoValido.estaValido = False
@@ -47,30 +39,22 @@ while (sair != 4):
         opcao1 = int(input("\t1. Alterar inscrição pelo CPF\n\t2. Alterar inscrição pelo código de inscrição\n=> "))
         if opcao1 == 1:
             cpf_alteracao = input("Informe seu CPF: ")
-            # o loop abaixo irá verificar aluno por aluno na lista alunos
             for aluno in alunos:
-                # verifica quando chega no cpf digitado
                 if aluno[2] == cpf_alteracao:
                     novo_estado = input("Digite o novo estado: ").upper()
                     estadoValido.validar(novo_estado)
                     novo_curso = cursoLista.cursos_por_estado(novo_estado)
-                    # troca o estado 
                     aluno[3] = novo_estado
-                    # troca o curso
                     aluno[4] = novo_curso
                     print("\n\033[32mAlteração finalizada com sucesso!\033[m")
         elif opcao1 == 2:
             codigo_alteracao = int(input("Informe o código de inscrição: "))
-            # o loop abaixo irá verificar aluno por aluno na lista alunos
             for aluno in alunos:
-                # verifica quando chega no codigo digitado
                 if aluno[0] == codigo_alteracao:
                     novo_estado = input("Digite o novo estado: ").upper()
                     estadoValido.validar(novo_estado)
                     novo_curso = cursoLista.cursos_por_estado(novo_estado)
-                    # troca o estado
                     aluno[3] = novo_estado
-                    # troca o curso
                     aluno[4] = novo_curso
                     print("\nAlteração finalizada!")
         else:
@@ -78,12 +62,9 @@ while (sair != 4):
 
     elif menu == 3:
         for codigoimport, cursoimport in cursoLista.cursos.items():
-            # a variável a baixo irá contabilizar a quantidade de alunos por curso
             qtd = 0
-            # head da visualização
             print(34 * '-')
             print(f" {codigoimport.ljust(0)} - {cursoimport}")
-            # o for irá exibir aluno por aluno de acordo com o curso
             for aluno in alunos:
                 if codigoimport == aluno[4]:
                     print(34 * '-')
