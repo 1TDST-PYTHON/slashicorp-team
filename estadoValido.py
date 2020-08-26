925cf8ed82abd5273e94b5b42b236ad2a93cc99b
Dihogo Cassimiro Teixeira <47646858+dihogoteixeira@users.noreply.github.com>
2020-08-26 20:58:24 -0300

    Update estadoValido.py

--- estadoValido.py
+++ estadoValido.py
@@ -1,13 +1,20 @@
+estadosCapitais = [("AC", "Rio Branco"), ("AL","Maceió"), ("AP","Macapá"), ("AM","Manaus"), ("BA","Salvador"), ("CE","Fortaleza"), ("DF","Brasília"), ("ES","Vitória"), ("GO","Goiânia"), ("MA","São Luís"), ("MT","Cuiabá"), ("MS","Campo Grande"), ("MG","Belo Horizonte"), ("PA","Belém"), ("PB","João Pessoa"), ("PR","Curitiba"), ("PE","Recife"), ("PI","Teresina"), ("RJ","Rio de Janeiro"), ("RN","Natal"), ("RS","Porto Alegre"), ("RO","Porto Velho"), ("RR","Boa Vista"), ("SC","Florianópolis"), ("SP","São Paulo"), ("SE","Aracaju"), ("TO","Palmas")]
 estaValido = False
-estados = ("AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SE", "SP", "TO")
-
 
 def validar(estado):
     global estaValido
-    if len(estado) == 2:
-        if estado in estados:
+    estado = estado.upper()
+    while len(estado) != 2:
+        estado = input("\033[31mDigite 2 caracteres para a sigla do Estado!\n\033[m").upper()
+    for index, estadoTemp in enumerate(estadosCapitais):
+        if estado == estadoTemp[0]:
             estaValido = True
-        else:
-            print("\033[31mEste Estado não existe!\033[m")
+            break
     else:
-        print("\033[31mDigite 2 caracteres para a sigla do Estado!\033[m")
+        estado = input("\033[31mEste Estado não existe! Digite outro estado:\n\033[m").upper()
+        validar(estado)
+
+def encontraCapital(estado):
+    for index, estadoTemp in enumerate(estadosCapitais):
+        if estado == estadoTemp[0]:
+            return estadosCapitais[index][1]
